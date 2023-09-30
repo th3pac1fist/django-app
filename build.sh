89da1e2df9d467f35ba28bfa2f3414fc1e3ef848
The Pacifist <105062485+th3pac1fist@users.noreply.github.com>
2023-09-30 17:45:17 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-django-admin startproject helloworld .
-echo 'from django.http import HttpResponse' > helloworld/views.py
-echo 'def hello(request):' >> helloworld/views.py
-echo '    return HttpResponse("Hello World!")' >> helloworld/views.py
-echo "from django.urls import path" > helloworld/urls.py
-echo "from . import views" >> helloworld/urls.py
-echo "urlpatterns = [path('', views.hello, name='hello'),]" >> helloworld/urls.py
+pip install -r requirements.txt
